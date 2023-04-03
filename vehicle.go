c91d01bc8ca2f5c33691dc6112011de636069eb6
pkk82@d2018-linux <piotrkrzysztofkozak@gmail.com>
2023-04-03 20:41:41 +0200

    Create `if` statement

--- vehicle.go
+++ vehicle.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 )
 
 type vehicle interface {
@@ -106,6 +107,10 @@ func generateRating() {
 		var vehResult feedbackResult
 		var vehRating rating
 		for _, msg := range v.Feedback {
+			text := strings.Split(msg, "")
+			if len(text) >= 5 {
+
+			}
 
 		}
 	}
