8347d2c02e5bde639dcf7e4d1ed56192aed0d58b
pkk82@d2018-linux <piotrkrzysztofkozak@gmail.com>
2023-04-03 20:47:49 +0200

    Create switch for feedback

--- vehicle.go
+++ vehicle.go
@@ -123,6 +123,14 @@ func generateRating() {
 						vehRating += extraNegative
 					}
 				}
+				switch {
+				case vehRating > 8.0:
+					vehResult.feedbackPositive++
+				case vehRating >= 4.0 && vehRating <= 8.0:
+					vehResult.feedbackNeutral++
+				case vehRating < 4.0:
+					vehResult.feedbackNegative++
+				}
 			}
 
 		}
