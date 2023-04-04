5bb690dd7b748deddca28460460fa4554a351fdd
pkk82@d2018-linux <piotrkrzysztofkozak@gmail.com>
2023-04-04 21:01:26 +0200

    Rename methods

--- vehicle.go
+++ vehicle.go
@@ -88,11 +88,11 @@ func main() {
 	for _, veh := range inventory {
 		switch v := veh.(type) {
 		case car:
-			v.showRating()
+			v.carDetails()
 		case bike:
-			v.showRating()
+			v.bikeDetails()
 		case truck:
-			v.showRating()
+			v.truckDetails()
 		default:
 			fmt.Printf("Are you sure this Vehicle Type exists")
 		}
@@ -165,17 +165,17 @@ func showRating(model string) {
 	}
 }
 
-func (c *car) showRating() {
+func (c *car) carDetails() {
 	fmt.Printf("\n%-5v: %-8v: %-12v ", "Car", c.make, c.model)
 	showRating(c.model)
 }
 
-func (b *bike) showRating() {
+func (b *bike) bikeDetails() {
 	fmt.Printf("\n%-5v: %-8v: %-12v ", "Bike", b.make, b.model)
 	showRating(b.model)
 }
 
-func (t *truck) showRating() {
+func (t *truck) truckDetails() {
 	fmt.Printf("\n%-5v: %-8v: %-12v ", "Truck", t.make, t.model)
 	showRating(t.model)
 }
